d64e26e1f7b744b3167957dfe5570981cd5e21b2
174 <174@E306PC02>
2023-11-10 14:53:10 +0000

    Adicionar arquivos de projeto.

---
+++ JokesWebApp2/Models/Jokes.cs
@@ -0,0 +1,17 @@
+﻿namespace JokesWebApp2.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public string JokePergunta { get; set; }
+        public string JokeResposta { get; set; }
+
+        public Jokes() 
+        {
+            Id = 0;
+            JokePergunta = string.Empty;
+            JokeResposta = string.Empty;
+        }
+    
+    }
+}
